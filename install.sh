bc4d5b673bf509b6a73e5c6f545895af1c983d44
Sam Nguyen <samuel.nguyen@panasonic.aero>
2011-04-19 13:29:33 -0700

    fixing install script

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 SCRIPT=`basename $0`
-DIR=`dirname $0`
-cd $DIR
+DIR=$(cd `dirname $0` && pwd)
+cd $HOME
 
-for FILE in `ls -d1 * | grep -v "$SCRIPT"`
+for FILE in `ls -1 "$DIR" | grep -v "$SCRIPT"`
 do
     DOTFILE=".$FILE"
     if [ -e "$HOME/$DOTFILE" ]
     then
         echo "x $DOTFILE"
     else
-        (cd ..; ln -s "$DIR/$FILE" "$DOTFILE")
+        ln -s "$DIR/$FILE" "$DOTFILE"
         [ $? -eq 0 ] && echo "+ $DOTFILE" || echo "? $DOTFILE"
     fi
 done
