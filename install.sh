91bab6776d9da09e179d86a2654c19ea4a6aa322
Sam Nguyen <samuel.nguyen@panasonic.aero>
2011-04-19 14:45:20 -0700

    adding rakefile check

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ SCRIPT=`basename $0`
 DIR=$(cd `dirname $0` && pwd)
 cd $HOME
 
+(cd $DIR; git submodule update --init)
+
 for FILE in `ls -1 "$DIR" | grep -v "$SCRIPT"`
 do
     DOTFILE=".$FILE"
@@ -13,5 +15,12 @@ do
     else
         ln -s "$DIR/$FILE" "$DOTFILE"
         [ $? -eq 0 ] && echo "+ $DOTFILE" || echo "? $DOTFILE"
+
+        # Run rake if necessary
+        if [ -e "$DIR/$FILE/Rakefile" ]
+        then
+            rake -f "$DIR/$FILE/Rakefile"
+        fi
     fi
+
 done
